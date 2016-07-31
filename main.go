79804f246bf8f2a1d4a2fbcb96ea34f89b1969dd
Andrew Arrow <andrew@0x7a69.net>
2016-07-31 00:31:54 +0000

    flag options, start of

--- main.go
+++ main.go
@@ -4,12 +4,36 @@ import "fmt"
 import "os"
 import "github.com/nlopes/slack"
 import "strconv"
+import "flag"
+
+var (
+	team = flag.String("team", "0", "team index zero based")
+	id   = flag.String("id", "0", "id")
+)
 
 func main() {
+	flag.Parse()
+
+	fmt.Println(*team)
+
+	if *id != "0" {
+		key := fmt.Sprintf("SLACK_TOKEN_%d", 0)
+		api := slack.New(os.Getenv(key))
+		hp := slack.HistoryParameters{Oldest: "", Latest: "", Count: 10, Inclusive: false, Unreads: false}
+		h, err := api.GetChannelHistory(*id, hp)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Println(h)
+		return
+	}
+
 	slack_teams, _ := strconv.ParseInt(os.Getenv("SLACK_TEAMS"), 10, 64)
 	i := int64(0)
 	for {
 		key := fmt.Sprintf("SLACK_TOKEN_%d", i)
+		fmt.Println(key)
 		api := slack.New(os.Getenv(key))
 		channels, err := api.GetChannels(false)
 		if err != nil {
