819223cde7d5c63989526f66cd9a7d9ba9c1590d
Andrew Arrow <andrew@0x7a69.net>
2016-07-31 01:07:07 +0000

    say

--- main.go
+++ main.go
@@ -7,14 +7,29 @@ import "strconv"
 import "flag"
 
 var (
-	team   = flag.String("team", "0", "team index zero based")
+	team   = flag.String("t", "0", "team index zero based")
 	id     = flag.String("id", "0", "id")
 	people = flag.String("p", "0", "list people")
+	say    = flag.String("s", "0", "say")
 )
 
 func main() {
 	flag.Parse()
 	slack_teams, _ := strconv.ParseInt(os.Getenv("SLACK_TEAMS"), 10, 64)
+
+	if *say != "0" {
+		key := fmt.Sprintf("SLACK_TOKEN_%s", *team)
+		api := slack.New(os.Getenv(key))
+		params := slack.PostMessageParameters{}
+		attachment := slack.Attachment{
+			Pretext: "2some pretext",
+			Text:    "3some text",
+		}
+		params.Attachments = []slack.Attachment{attachment}
+		_, _, _ = api.PostMessage(*id, "1Some text", params)
+
+		return
+	}
 
 	if *people != "0" {
 		i := int64(0)
