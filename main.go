dc743123b69b8e5bac8605c17c36e6054ad3d975
Andrew Arrow <andrew@0x7a69.net>
2016-07-31 00:12:17 +0000

    support multiple teams

--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 import "os"
 import "github.com/nlopes/slack"
+import "strconv"
 
 func main() {
-	api := slack.New(os.Getenv("SATOK"))
-	// If you set debugging, it will log all requests to the console
-	// Useful when encountering issues
-	// api.SetDebug(true)
-	groups, err := api.GetGroups(true)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
+	slack_teams, _ := strconv.ParseInt(os.Getenv("SLACK_TEAMS"), 10, 64)
+	i := int64(0)
+	for {
+		key := fmt.Sprintf("SLACK_TOKEN_%d", i)
+		api := slack.New(os.Getenv(key))
+		channels, err := api.GetChannels(false)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		for _, channel := range channels {
+			fmt.Println(channel.ID, channel.Name)
+		}
+
+		i++
+		if i >= slack_teams {
+			break
+		}
 	}
-	for _, group := range groups {
-		fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
-	}
+
 }
