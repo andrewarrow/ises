d5e40353f46121ba3292c339b854e4195369b24f
Andrew Arrow <andrew@0x7a69.net>
2016-08-01 04:54:51 +0000

    date

--- read_mode.go
+++ read_mode.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 import "io/ioutil"
+import "strings"
+import "strconv"
+import "time"
 
 func handleReadMode() {
 	files, _ := ioutil.ReadDir("cache/")
 	for _, f := range files {
 		fmt.Println(f.Name())
+		subfiles, _ := ioutil.ReadDir("cache/" + f.Name())
+		for _, sub := range subfiles {
+			tokens := strings.Split(sub.Name(), "_")
+			fmt.Println(tokens[0])
+			subtokens := strings.Split(tokens[0], ".")
+			number, _ := strconv.ParseInt(subtokens[0], 10, 0)
+			t := time.Unix(int64(number), 0)
+			fmt.Println(t)
+		}
 	}
 }
