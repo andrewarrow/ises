56d9f6b8cf637a21e955310064fd2596e9396916
Andrew Arrow <andrew@0x7a69.net>
2016-08-02 03:23:58 +0000

    mark

--- room/mark.go
+++ room/mark.go
@@ -1,4 +1,24 @@
 package room
 
+import "io/ioutil"
+import "fmt"
+import "os"
+
 func MarkRoomWithRedDot(name, team string) {
+	alpha := []string{"a", "b", "c", "d", "e",
+		"f", "g", "h"}
+	// write out bash sh named a_name, b_name
+	files, _ := ioutil.ReadDir("ui/")
+	size := len(files)
+
+	if size > 25 {
+		return
+	}
+
+	fstr := fmt.Sprintf("ui/%s_%s", alpha[size], name)
+	cmd := fmt.Sprintf("TEAM=%s ROOM=%s ../ises -r", team, name)
+	f, _ := os.OpenFile(fstr, os.O_CREATE|os.O_WRONLY, 0660)
+	_, _ = f.WriteString(cmd)
+	f.Close()
+
 }
