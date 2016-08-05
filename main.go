a15bf1a4f9b7d2047beaf76b3daab7174018c358
Andrew Arrow <andrew@0x7a69.net>
2016-08-05 02:26:07 +0000

    working

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 import gc "github.com/rthornton128/goncurses"
+import "time"
 
 var (
 	IsesRoot string
+	stdscr   *gc.Window
+	history  []string
 )
 
+func thready() {
+	for {
+		time.Sleep(time.Second)
+		for row, h := range history {
+			stdscr.MovePrint(row, 0, h)
+		}
+		stdscr.Refresh()
+	}
+}
+
 func main() {
-	stdscr, _ := gc.Init()
+	history = make([]string, 0)
+	stdscr, _ = gc.Init()
+	go thready()
 	gc.Echo(false)
 	//gc.Raw(true)
 	stdscr.Keypad(true)
@@ -22,7 +37,8 @@ func main() {
 		c := stdscr.GetChar()
 		//stdscr.MovePrintf(15, 10, "|%d|", c)
 		if c == 10 || c == 13 {
-			stdscr.MovePrintf(10, 10, "%s", line)
+			//stdscr.MovePrintf(10, 10, "%s", line)
+			history = append(history, line)
 			buff = make([]byte, 0)
 			stdscr.MovePrint(row-1, 0, "                                                                   ")
 			stdscr.MovePrint(row-1, 0, "> ")
