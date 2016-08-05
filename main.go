3f16f372d3c9e76a10f8e8b9517cdae19ecf88d8
Andrew Arrow <andrew@0x7a69.net>
2016-08-05 23:12:33 +0000

    next

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 import gc "github.com/rthornton128/goncurses"
 import "time"
 import "os"
+import "sort"
 import "github.com/nlopes/slack"
 import "github.com/andrewarrow/ises/room"
 import "strings"
@@ -16,7 +17,20 @@ var (
 	realId   string
 	line     string
 	curPos   int
+	curRoom  int
+	recent   map[string]int64
 )
+
+type RecentRoom struct {
+	ts   int64
+	name string
+}
+
+type ByRoomAge []RecentRoom
+
+func (a ByRoomAge) Len() int           { return len(a) }
+func (a ByRoomAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByRoomAge) Less(i, j int) bool { return a[i].ts < a[j].ts }
 
 func thready(maxrows int) {
 	for {
@@ -42,6 +56,7 @@ func handleRtmInCurses(rtm *slack.RTM, team string) {
 			switch ev := msg.Data.(type) {
 			case *slack.MessageEvent:
 				name := room.IdToString(ev.Msg.Channel, team)
+				recent[team+"_"+name] = time.Now().Unix()
 				h := make(map[string]string)
 				h["text"] = ev.Msg.Text
 				h["time"] = ev.Msg.Timestamp
@@ -61,12 +76,37 @@ func handleRtmInCurses(rtm *slack.RTM, team string) {
 
 }
 
+func findRecents() []RecentRoom {
+	list := make([]RecentRoom, 0)
+	for k, v := range recent {
+		rr := RecentRoom{}
+		rr.ts = v
+		rr.name = k
+		list = append(list, rr)
+	}
+	sort.Sort(ByRoomAge(list))
+	return list
+
+	/*
+		fstr := "log.log"
+		f, _ := os.OpenFile(fstr, os.O_APPEND|os.O_WRONLY, 0660)
+		for _, item := range list {
+			_, _ = f.WriteString(item.name + "\n")
+		}
+		f.Close()
+	*/
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 {
 		fmt.Println("./ises rid")
 		return
 	}
+	recent = make(map[string]int64)
+	recent["1_for_andrew"] = time.Now().Unix()
+	recent["0_aa"] = time.Now().Unix()
+	recent["2_jasoncarulli"] = time.Now().Unix()
 	tokens := strings.Split(args[0], ".")
 	team_str := tokens[0]
 	rid = tokens[1]
@@ -98,6 +138,7 @@ func main() {
 	buff := make([]byte, 0)
 	line = ""
 	curPos = 0
+	curRoom = 0
 	for {
 		c := stdscr.GetChar()
 		//stdscr.MovePrintf(15, 10, "|%d|", c)
@@ -114,11 +155,18 @@ func main() {
 			stdscr.MovePrint(row-1, 0, rid+"> ")
 			stdscr.Refresh()
 		} else if c == 93 {
-			rid = "aa"
-			realId = room.StringToId(rid, "0")
+			list := findRecents()
+			curRoom++
+			if curRoom >= len(list) {
+				curRoom = 0
+			}
+			tmp := list[curRoom].name
+			rid = tmp[2:len(tmp)]
+			newTeam := tmp[0:1]
+			realId = room.StringToId(rid, newTeam)
 			teams := room.GetTeams()
 			for _, t := range teams {
-				if "0" != t.Index {
+				if newTeam != t.Index {
 					continue
 				}
 				team = t
