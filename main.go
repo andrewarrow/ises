b88fb5cb94a98f98e82cac30951cdfea4a7db3f5
Andrew Arrow <andrew@0x7a69.net>
2016-08-05 02:13:36 +0000

    uparrow

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ var (
 func main() {
 	stdscr, _ := gc.Init()
 	gc.Echo(false)
+	//gc.Raw(true)
+	stdscr.Keypad(true)
 	row, _ := stdscr.MaxYX()
 
 	stdscr.MovePrint(row-1, 0, "> ")
@@ -18,7 +20,7 @@ func main() {
 	line := ""
 	for {
 		c := stdscr.GetChar()
-		//stdscr.MovePrintf(15, 10, "%d", c)
+		//stdscr.MovePrintf(15, 10, "|%d|", c)
 		if c == 10 || c == 13 {
 			stdscr.MovePrintf(10, 10, "%s", line)
 			buff = make([]byte, 0)
@@ -31,7 +33,12 @@ func main() {
 				break
 			}
 		} else {
-			if c == 127 {
+			nice := gc.KeyString(c)
+			if nice == "up" {
+			} else if nice == "down" {
+			} else if nice == "left" {
+			} else if nice == "right" {
+			} else if c == 127 {
 				if len(buff) > 0 {
 					buff = buff[0 : len(buff)-1]
 					line = string(buff)
