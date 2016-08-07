9fd8f4c5721eeeb33eddc0ef92fa020149e0309d
Andrew Arrow <andrew@0x7a69.net>
2016-08-07 02:30:54 +0000

    users

--- soeasy/setup.go
+++ soeasy/setup.go
@@ -17,7 +17,27 @@ func SoEasySetup() {
 	}
 	for _, team := range teams {
 		fmt.Println("Caching users from team ", team.Index)
-
+		cache_users(team)
 	}
 
+}
+
+func cache_users(team room.Team) {
+	users, _ := team.Api.GetUsers()
+	for _, user := range users {
+		dir1 := "cache/id_lookup/" + team.Index + "/"
+		dir2 := "cache/reverse_lookup/" + team.Index + "/"
+
+		_ = os.MkdirAll(dir1, os.ModePerm)
+		_ = os.MkdirAll(dir2, os.ModePerm)
+
+		fstr1 := dir1 + user.ID
+		fstr2 := dir2 + user.Name
+		f, _ := os.OpenFile(fstr1, os.O_CREATE|os.O_WRONLY, 0660)
+		_, _ = f.WriteString(user.Name)
+		f.Close()
+		f, _ = os.OpenFile(fstr2, os.O_CREATE|os.O_WRONLY, 0660)
+		_, _ = f.WriteString(user.ID)
+		f.Close()
+	}
 }
